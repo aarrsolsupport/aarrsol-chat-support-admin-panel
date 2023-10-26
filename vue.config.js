4f052aa6b13ccfaec7bb227661227b1620366ed0
avanias <avani@aarrsolprivatelimited.com>
2023-10-26 14:44:57 +0530

    Language based Chat flow templates updates

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: 'chat-system-admin.dll',
+    port: 8080,
+    https: false
+  }
 })

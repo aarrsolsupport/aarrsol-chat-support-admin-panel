3ac2f98ad2f070a114734175e7955fc4f088b16f
avanias <avani@aarrsolprivatelimited.com>
2023-12-05 12:17:41 +0530

    Media preview default & message time updates

--- src/helpers/filters.js
+++ src/helpers/filters.js
@@ -1,6 +1,12 @@
 import moment from 'moment';
 import 'moment-timezone';
 const filters = {
+  messageDisplayDateFormat(datetime) {
+    return moment.unix(datetime).format('ddd, D MMM');
+  },
+  messageDisplayTimeFormat(datetime) {
+    return moment.unix(datetime).format('hh:mm A');
+  },
   messageDateTimeFormat(datetime) {
     return moment.unix(datetime).format('DD/MM/YYYY hh:mm A');
   },

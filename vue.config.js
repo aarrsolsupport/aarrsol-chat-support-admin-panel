fe4b063306950645208986cab60aae9ca8b6b042
root <root@ip-10-0-1-163.ap-south-1.compute.internal>
2023-12-08 06:44:30 +0000

    server setup for master

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: 'chat-system-admin.dll',
-    port: 8080,
-    https: false
-  }
+    host: process.env.VUE_APP_HOST,
+    port: process.env.VUE_APP_ADMIN_PORT,
+    https: false,
+    client: {
+      webSocketURL: {
+        hostname: process.env.VUE_APP_WEBSOCKETS_SERVER,
+        port: 2096,
+        pathname: '',
+      },
+    }
+  },
+  lintOnSave: false
 })
